5bfebd47b8feeb1dacc2545ca3666ec9e0e6df73
Luis Fernando Muchau <nandomuchau@gmail.com>
2018-04-26 19:38:20 -0700

    Link the date to the view

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,18 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        TextView mOriginTv = (TextView) findViewById(R.id.origin_tv);
+        mOriginTv.setText(sandwich.getPlaceOfOrigin());
+
+        TextView mDescriptionTv = (TextView) findViewById(R.id.description_tv);
+        mDescriptionTv.setText(sandwich.getDescription());
+
+        TextView mIngredientsTV = (TextView) findViewById(R.id.ingredients_tv);
+        mIngredientsTV.setText(sandwich.getIngredientsString());
+
+        TextView mAlsoKnownTv = (TextView) findViewById(R.id.also_known_tv);
+        mAlsoKnownTv.setText(sandwich.getAlsoKnownAsString());
     }
 }
