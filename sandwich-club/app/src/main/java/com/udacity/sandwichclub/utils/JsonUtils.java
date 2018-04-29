1e2e2294aea0f3b164309d806c8e5a08b56b25c0
Luis Fernando Muchau <nandomuchau@gmail.com>
2018-04-29 11:08:06 -0700

    Add constants for variable names and replace get to opt to be sure thaat will return a empty string if there is no value.

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,30 +11,38 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String JSON_NAME_KEY = "name";
+    public static final String JSON_MAIN_NAME_KEY = "mainName";
+    public static final String JSON_ALSO_KNOWN_AS_KEY = "alsoKnownAs";
+    public static final String JSON_PLACE_OF_ORIGIN_KEY = "placeOfOrigin";
+    public static final String JSON_DESCRIPTION_KEY = "description";
+    public static final String JSON_IMAGE_KEY = "image";
+    public static final String JSON_INGREDIENTS_KEY = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
 
         try {
             JSONObject sandwichObject = new JSONObject(json);
 
-            JSONObject nameObject = sandwichObject.getJSONObject("name");
-            String mainName = nameObject.getString("mainName");
+            JSONObject nameObject = sandwichObject.optJSONObject(JSON_NAME_KEY);
+            String mainName = nameObject.optString(JSON_MAIN_NAME_KEY);
 
-            JSONArray alsoKnownAsArray = nameObject.getJSONArray("alsoKnownAs");
+            JSONArray alsoKnownAsArray = nameObject.optJSONArray(JSON_ALSO_KNOWN_AS_KEY);
             List<String> alsoKnownAs = new ArrayList<>();
             for(int i = 0; i < alsoKnownAsArray.length(); i++){
-                alsoKnownAs.add(alsoKnownAsArray.getString(i));
+                alsoKnownAs.add(alsoKnownAsArray.optString(i));
             }
 
-            String placeOfOrigin = sandwichObject.getString("placeOfOrigin");
+            String placeOfOrigin = sandwichObject.optString(JSON_PLACE_OF_ORIGIN_KEY);
 
-            String description = sandwichObject.getString("description");
+            String description = sandwichObject.optString(JSON_DESCRIPTION_KEY);
 
-            String image = sandwichObject.getString("image");
+            String image = sandwichObject.optString(JSON_IMAGE_KEY);
 
-            JSONArray ingredientsArray = sandwichObject.getJSONArray("ingredients");
+            JSONArray ingredientsArray = sandwichObject.optJSONArray(JSON_INGREDIENTS_KEY);
             List<String> ingredients = new ArrayList<>();
             for(int i = 0; i < ingredientsArray.length(); i++){
-                ingredients.add(ingredientsArray.getString(i));
+                ingredients.add(ingredientsArray.optString(i));
             }
 
             return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image,
