c0e1765a2c7291215aa6b993aaeb96719b25e63e
Luis Fernando Muchau <nandomuchau@gmail.com>
2018-04-29 16:36:54 -0700

    Add error and placeholder image for Picasso

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -47,6 +47,8 @@ public class DetailActivity extends AppCompatActivity {
         populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
